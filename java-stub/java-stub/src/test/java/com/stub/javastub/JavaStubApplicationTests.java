592bab95ce98defcfb0f3243da219230c1256be9
Irina Shilova <ya@ishilova.ru>
2026-06-16 04:26:03 +0300

    Add template Java Stub

---
+++ java-stub/java-stub/src/test/java/com/stub/javastub/JavaStubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stub.javastub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaStubApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
